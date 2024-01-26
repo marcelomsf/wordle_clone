5f36407d914a1a5025fa40899457a07281886f28
Marcelo Franco <mfranco@australholding.com>
2024-01-26 14:13:01 -0300

    UPDATE arquivo  wordle.js -FIX for command

--- wordle.js
+++ wordle.js
@@ -5,7 +5,7 @@ var row = 0; //current guess (attempt #)
 var col = 0; //current letter for that attempt
 
 var gameOver = false;
-var word ="SQUID"
+var word ="SQUID";
 
 window.onload = function(){
     intialize();
@@ -14,14 +14,14 @@ window.onload = function(){
 function intialize() {
 
     //create the game board
-    for (let r= 0; t < height; r++ ){
+    for (let r= 0; r < height; r++ ){
         for (let c = 0; c < width; c++ ){
             // <span id="0-0" class"title">P</span>
             let tile = document.createElement("span");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
             tile.innerText = "P";
-            document.getElementById("board").appendChild(tile)
+            document.getElementById("board").appendChild(tile);
         }
     }
 
